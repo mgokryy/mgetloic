30bacce89616b588a98bb3ec47e7286f8980e337
loictossou2004 <loic.tossou@epitech.eu>
2024-04-10 23:50:02 +0200

    Ajout de la fonction add_quote dans le fichier functions.py

--- functions.py
+++ functions.py
@@ -1,5 +1,12 @@
 import random
 
+def add_quote(quotes, filename):
+    new_quote = input("Enter a new quote: ")
+    quotes.append(new_quote)
+
+    with open(filename, 'a') as file:
+        file.write(new_quote)
+
 def load_quotes(filename):
     quotes = []
     with open(filename, 'r') as file:
